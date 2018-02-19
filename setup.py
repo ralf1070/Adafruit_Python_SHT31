6a7de26f8878bb2878a19f4fbcdb1feac7884a3c
Ralf Müller <ralf@bj-ig.de>
2018-02-19 14:34:31 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Adafruit_SHT31',
+      version='1.0',
+      description='Adafruit Python SHT31 Liibrary',
+      url='https://github.com/ralf1070/Adafruit_Python_SHT31',
+      py_modules=['Adafruit_SHT31'],
+     )
